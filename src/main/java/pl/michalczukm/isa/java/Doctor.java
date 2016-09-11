f1535c379703c1d69e6b8f6ff7e28c9a0bda490a
Marcin P <marcin.pankowski@gmail.com>
2016-09-11 18:12:22 +0200

    doctor service feature - constructor

--- src/main/java/pl/michalczukm/isa/java/Doctor.java
+++ src/main/java/pl/michalczukm/isa/java/Doctor.java
@@ -4,6 +4,9 @@ package pl.michalczukm.isa.java;
  * Created by marcinpankowski on 11.09.16.
  */
 public class Doctor {
+    public void Doctor(){
+
+    }
     public static void main(String[] args) {
 
     }
