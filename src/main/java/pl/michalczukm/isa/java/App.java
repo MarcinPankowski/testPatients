68385ec9d5865a410e6ade13814f6dd4a85b3915
Marcin P <marcin.pankowski@gmail.com>
2016-09-18 16:57:52 +0200

    comment

--- src/main/java/pl/michalczukm/isa/java/App.java
+++ src/main/java/pl/michalczukm/isa/java/App.java
@@ -10,6 +10,7 @@ public class App
         PatientModel patientModel = new PatientModel();
         DoctorService doctorService = new DoctorService();
         List<PatientApp> patientApps = patientModel.getAll();
+        //coment to revert
 
         for (PatientApp patientApp : patientApps) {
             System.out.println(String.format(">> %1$s %2$s <<", patientApp.getFirstName(), patientApp.getLastName()));
