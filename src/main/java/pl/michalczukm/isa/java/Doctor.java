2f1e4ae64f5aadaa4199d5303ae99347443e100f
Marcin P <marcin.pankowski@gmail.com>
2016-09-18 16:15:19 +0200

    changes in Doctors

--- src/main/java/pl/michalczukm/isa/java/Doctor.java
+++ src/main/java/pl/michalczukm/isa/java/Doctor.java
@@ -4,8 +4,28 @@ package pl.michalczukm.isa.java;
  * Created by marcinpankowski on 11.09.16.
  */
 public class Doctor {
-    public void Doctor(){
+    private String imie;
+    private String nazwisko;
 
+    public Doctor(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
     }
 
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
 }
--- src/main/java/pl/michalczukm/isa/java/DoctorService.java
+++ src/main/java/pl/michalczukm/isa/java/DoctorService.java
@@ -5,6 +5,6 @@ package pl.michalczukm.isa.java;
  */
 public class DoctorService {
     public static void main(String[] args) {
-
+        new Doctor("Marcin","Kowalski");
     }
 }
