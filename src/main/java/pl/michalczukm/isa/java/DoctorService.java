3bde23caa72262d7ea01ef324beb0f638a6a80a6
Marcin P <marcin.pankowski@gmail.com>
2016-09-18 17:35:56 +0200

    bad bad bad name

--- src/main/java/pl/michalczukm/isa/java/DoctorService.java
+++ src/main/java/pl/michalczukm/isa/java/DoctorService.java
@@ -7,5 +7,8 @@ public class DoctorService {
     public static void main(String[] args) {
         new Doctor("Marcin","Kowalski");
         new Doctor("Zbigniew","Marcinkowski");
+        new Doctor("Zb","Marc");
+        new Doctor("Zbig","Marcink");
+        new Doctor("Zbig","Marcink");
     }
 }
