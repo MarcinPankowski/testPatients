a3ae86ab1cc93b7aaae20a8ab35c04bfaf9a515b
Marcin P <marcin.pankowski@gmail.com>
2016-09-18 15:32:39 +0200

    new field for Patient - age

--- src/main/java/pl/michalczukm/isa/java/Patient.java
+++ src/main/java/pl/michalczukm/isa/java/Patient.java
@@ -3,10 +3,13 @@ package pl.michalczukm.isa.java;
 public class Patient {
     private final String firstName;
     private final String lastName;
+    private int age;
 
-    public Patient(String firstName, String lastName) {
+
+    public Patient(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.age = age;
     }
 
     public String getFirstName() {
@@ -15,5 +18,13 @@ public class Patient {
 
     public String getLastName() {
         return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
