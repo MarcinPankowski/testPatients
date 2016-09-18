5422fa2c4c8dfba049d5b03eb0a7067513856758
Marcin P <marcin.pankowski@gmail.com>
2016-09-18 17:15:46 +0200

    add visits services

---
+++ src/main/java/pl/michalczukm/isa/java/Visits.java
@@ -0,0 +1,10 @@
+package pl.michalczukm.isa.java;
+
+/**
+ * Created by marcinpankowski on 18.09.16.
+ */
+public class Visits {
+    public void Visits(){
+        System.out.println("Many Visits");
+    }
+}
